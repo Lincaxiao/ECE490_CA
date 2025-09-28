384a785ed159aa4d44cec8d24b7b5ee5ab2deca4
xiaochen <xiaochen.23@intl.zju.edu.cn>
2025-09-27 20:16:43 -0500

    Add the comparison between different step size strategies.

--- CA1/CA1.py
+++ CA1/CA1.py
@@ -27,12 +27,18 @@ def find_inverse(Q):
     return np.linalg.inv(Q)
 
 # Task 1: Constant step size
-def grad_descent_with_constant_step(Q, b, c, epsilon, x0=None, step_scale=1):
+def grad_descent_with_constant_step(Q, b, c, epsilon, x0=None, step_scale=1, size_type=1):
     # clamp step_scale to be in (0,2)
     step_scale = max(1e-8, min(step_scale, 2-1e-8))
     # compute step size, between (0, 2/L)
-    L, _ = find_max_and_min_eigenvalue(Q)
-    step_size = step_scale * 1 / L
+    L, m = find_max_and_min_eigenvalue(Q)
+    # choose step size strategy
+    if size_type == 1:
+        step_size = step_scale / L
+    elif size_type == 2:
+        step_size = m / L**2 # pessimistic and loose step size
+    elif size_type == 3:
+        step_size = 2 / (m + L) # optimal constant step size for quadratic functions
     if x0 is None:
         x0 = np.zeros(Q.shape[0])
     k = 0 # iteration counter
@@ -44,8 +50,8 @@ def grad_descent_with_constant_step(Q, b, c, epsilon, x0=None, step_scale=1):
         x_cur -= step_size * grad_cur # gradient descent step
         grad_cur = cal_grad_f(x_cur, Q, b)
         k += 1 # increment iteration counter
-    # return x*, k, f(x*), ||grad_f(x*)||_2, L
-    return x_cur, k, cal_f(x_cur, Q, b, c), np.linalg.norm(cal_grad_f(x_cur, Q, b), ord=2), L
+    # return x*, k, f(x*), ||grad_f(x*)||_2, L, m
+    return x_cur, k, cal_f(x_cur, Q, b, c), np.linalg.norm(cal_grad_f(x_cur, Q, b), ord=2), L, m
 
 #Task 2: Armijo's rule
 def find_alpha_with_armijo(Q, b, c, x_k, beta=0.5):
@@ -97,10 +103,13 @@ if __name__ == "__main__":
     epsilon = 1e-5
     Q, b, c = get_Q_b_c(n)
     x0 = np.random.rand(n)
+    # m/L^2 is a pessimistic and loose step size for all functions satisfying
+    # m-strong convexity and L-smoothness
+    # 2 / (m + L) is the optimal constant step size specific for quadratic functions
     step_scale = 1 # can be between (0,2), for task 1
     beta = 0.5 # theoretically can be between (0,1), for task 2
     # Task 1
-    x_task1, k_task1, f_task1, grad_norm_task1, L = grad_descent_with_constant_step(Q, b, c, epsilon, x0)
+    x_task1, k_task1, f_task1, grad_norm_task1, L, m = grad_descent_with_constant_step(Q, b, c, epsilon, x0, step_scale, 1)
     # Task 2
     x_task2, k_task2, f_task2, grad_norm_task2 = grad_descent_with_armijo(Q, b, c, epsilon, x0, beta)
     # Task 3
@@ -112,7 +121,7 @@ if __name__ == "__main__":
     
     # print task 1
     print("\n=== Task1: Constant Step Size Gradient Descent ===")
-    print(f"L  = {float(L)}, step_scale = {float(step_scale)}")
+    print(f"L  = {float(L)}, m = {float(m)}, step_scale = {float(step_scale)}")
     print(f"iterations = {k_task1}")
     print(f"f(x*) = {float(f_task1.item())}")
     print(f"x* = {x_task1.astype(float)}")
@@ -130,3 +139,30 @@ if __name__ == "__main__":
     print("\n=== Task3: Matrix Inversion ===")
     print(f"f(x*) = {float(f_task3.item())}")
     print(f"x* = {x_task3.astype(float)}")
+
+
+    # Make comparisons between different step size strategies
+    # Type 1 has been done above
+    # Type 2: m/L^2
+    x_task1_2, k_task1_2, f_task1_2, grad_norm_task1_2, _, _ = grad_descent_with_constant_step(Q, b, c, epsilon, x0, step_scale, 2)
+    # Type 3: 2/(m+L)
+    x_task1_3, k_task1_3, f_task1_3, grad_norm_task1_3, _, _ = grad_descent_with_constant_step(Q, b, c, epsilon, x0, step_scale, 3)
+    # print comparisons
+    print("\n=== Comparison of Different Step Size Strategies ===")
+    print("\n=== Step Size = step_scale / L ===")
+    print(f"iterations = {k_task1}")
+    print(f"f(x*) = {float(f_task1.item())}")
+    print(f"x* = {x_task1.astype(float)}")
+    print(f"||grad_f(x*)||_2 = {float(grad_norm_task1)}")
+    # m/L^2
+    print("\n=== Step Size = m / L^2 ===")
+    print(f"iterations = {k_task1_2}")
+    print(f"f(x*) = {float(f_task1_2.item())}")
+    print(f"x* = {x_task1_2.astype(float)}")
+    print(f"||grad_f(x*)||_2 = {float(grad_norm_task1_2)}")
+    # 2/(m+L)
+    print("\n=== Step Size = 2 / (m + L) ===")
+    print(f"iterations = {k_task1_3}")
+    print(f"f(x*) = {float(f_task1_3.item())}")
+    print(f"x* = {x_task1_3.astype(float)}")
+    print(f"||grad_f(x*)||_2 = {float(grad_norm_task1_3)}")
